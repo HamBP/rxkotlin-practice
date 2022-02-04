33d16921c8abe3cadff9bf3ca841994debe58371
algosketch <sjy_9484@naver.com>
2022-02-04 22:43:22 +0900

    add observable

---
+++ src/main/java/observable.kt
@@ -0,0 +1,59 @@
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+import io.reactivex.observables.ConnectableObservable
+import io.reactivex.rxkotlin.toObservable
+
+fun main(args: Array<String>) {
+}
+
+fun observable1() {
+    val list = listOf(2, 1, 3, 4, 5, 6)
+    val observable = list.toObservable()
+    val observer = object : Observer<Any> {
+        override fun onSubscribe(d: Disposable) {
+            println("new subscription")
+        }
+        override fun onNext(t: Any) {
+            println("on next called! : $t")
+        }
+        override fun onError(e: Throwable) {
+            println("error!")
+        }
+        override fun onComplete() {
+            println("on complete")
+        }
+    }
+    observable.subscribe(observer)
+}
+
+fun observable2() {
+    val list = listOf(2, 1, 3, 4, 5, 6)
+    val observable = list.toObservable()
+    observable.subscribe {
+        println("on next : $it")
+    }
+}
+
+fun observable3() {
+    val list = listOf(2, 1, 3, 4, 5, 6)
+    val observable = Observable.fromIterable(list)
+    observable.subscribe {
+        println("on next : $it")
+    }
+}
+
+fun observable4() {
+    val observable = Observable.just(3, 2, 1, 4, 5)
+    observable.subscribe {
+        println("on next : $it")
+    }
+}
+
+fun observable5() {
+    val list = listOf(2, 1, 3, 4, 5, 6)
+    val observable = list.toObservable().publish()
+    observable.subscribe { println("on next called! : $it") }
+    observable.connect()
+    observable.subscribe { println("on next called!! : $it") }
+}
